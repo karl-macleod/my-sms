a23e3bcfb37ebbfa3da7521b423b7259459831a3
Karl Macleod <karl.macleod@readytech.io>
2021-04-16 13:56:03 +1000

    Implement suggestions. - Add line breaks to Course spec as suggested. - Split validation tests into separate tests, instead of having one test. - Update Course factory to support generating nils for relevant Course attributes.

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :course do
     name { Faker::Educator.course_name }
-    description { Faker::Lorem.paragraph(sentence_count: 5) }
+    description { [nil, '', Faker::Lorem.paragraph(sentence_count: 5)].sample }
     number_of_semesters { Faker::Number.within(range: 1..4) }
-    allocation { Faker::Number.within(range: 1..100) }
+    allocation { [nil, Faker::Number.within(range: 1..100)].sample }
     start_date { Faker::Date.backward(days: 180) }
     end_date { Faker::Date.forward(days: 180) }
   end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -2,25 +2,70 @@ require 'rails_helper'
 
 RSpec.describe Course, type: :model do
   subject { course }
-  context 'all fields contain valid values' do
-    let(:course) { build(:course, name: 'Intro to Valid Course Names 101', description: 'A fabulous course that everybody should do.', number_of_semesters: 2, start_date: Date.new(2020, 1, 1),  end_date: Date.new(2020, 12, 31), allocation: 98) }
+  context 'when all fields contain valid values' do
+    let(:course) { build(:course,
+      name: 'Intro to Valid Course Names 101',
+      description: 'A fabulous course that everybody should do.',
+      number_of_semesters: 2,
+      start_date: Date.new(2020, 1, 1),
+      end_date: Date.new(2020, 12, 31),
+      allocation: 98) }
     
     it { is_expected.to be_valid }
+
+    it 'allows a blank description' do
+      subject.description = ''
+
+      expect(subject).to be_valid
+    end
+
+    it 'allows no student allocation' do
+      subject.allocation = nil
+
+      expect(subject).to be_valid
+    end
   end
-  context 'has fields that contain invalid values' do
-    let(:course) { build(:course, name: nil, description: 'A terrible course with lots of problems.', number_of_semesters: -2, start_date: nil,  end_date: nil, allocation: -2) }
+  context 'when fields have invalid values' do
+    let(:course) { build(:course) }
 
-    it { is_expected.to be_invalid }
+    it 'has an error for a blank name' do
+      subject.name = ''
 
-    it 'has the expected validation errors' do
       subject.valid?
+
       expect(subject.errors[:name].count).to be > 0
+    end
+
+    it 'has an error for a negative number of semesters' do
+      subject.number_of_semesters = -1
+
+      subject.valid?
+
       expect(subject.errors[:number_of_semesters].count).to be > 0
+    end
+
+    it 'has an error when there is no start date' do
+      subject.start_date = nil
+
+      subject.valid?
+
       expect(subject.errors[:start_date].count).to be > 0
+    end
+    
+    it 'has an error when there is no end date' do
+      subject.end_date = nil
+
+      subject.valid?
+
       expect(subject.errors[:end_date].count).to be > 0
+    end
+
+    it 'has an error for a negative student allocation' do
+      subject.allocation = -1
+
+      subject.valid?
+
       expect(subject.errors[:allocation].count).to be > 0
-      # Description has no validation requirements
-      expect(subject.errors[:description].count).to be == 0
     end
   end
 end
